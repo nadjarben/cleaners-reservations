57b3498938b203965f2edc778217d0d677ca7599
nadjarben <nadjarbenj@gmail.com>
2019-05-20 21:58:22 +0200

    i am

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,2 +1,7 @@
+self.addEventListener('install', function(event) {
+    console.log('Installed sw.js', event);
+});
 
-  
+self.addEventListener('activate', function(event) {
+    console.log('Activated sw.js', event);
+});
