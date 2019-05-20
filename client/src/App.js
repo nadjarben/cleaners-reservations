24327651af30e9b11b385bc4567281c66b752843
nadjarben <nadjarbenj@gmail.com>
2019-05-20 12:34:05 +0200

    make it

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {  Router, Route } from 'react-router-dom';
 import Intro from './containers/Intro.js'; 
 import Reservation from './containers/Reservation.js'; 
 import Home from './containers/Home.js'; 
