89df6021b0b1ca608ca29f992b3edf39903f0d15
nadjarben <nadjarbenj@gmail.com>
2019-05-21 15:24:57 +0200

    i am

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
-import store from './store';
+import store from './store'
 import { addLocaleData } from 'react-intl';
 import en from 'react-intl/locale-data/en';
 import fr from 'react-intl/locale-data/fr';
