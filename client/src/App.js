e9d0167e1d7783c8250db1b02eec67c30aae514d
nadjarben <nadjarbenj@gmail.com>
2019-05-21 12:34:58 +0200

    i am

--- client/src/App.js
+++ client/src/App.js
@@ -18,20 +18,7 @@ class App extends Component {
   
 
   render() {
-
-    // Detects if device is on iOS 
-const isIos = () => {
-  const userAgent = window.navigator.userAgent.toLowerCase();
-  return /iphone|ipad|ipod/.test( userAgent );
-}
-// Detects if device is in standalone mode
-const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
-
-// Checks if should display install popup notification:
-if (isIos() && !isInStandaloneMode()) {
-  this.setState({ showInstallMessage: true });
-}
-
+    
   //  const history = configureHistory()
     const { lang } = this.props;
     return( 
