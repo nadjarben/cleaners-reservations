d3d4b8a5e5ff406a8d4731d78fd70a2808b8d498
nadjarben <nadjarbenj@gmail.com>
2019-05-21 12:04:27 +0200

    i am

--- client/src/index.js
+++ client/src/index.js
@@ -17,17 +17,6 @@ addLocaleData(he);
 if (localStorage.alhubLang) {
     store.dispatch(localeSet(localStorage.alhubLang)
     )};
-    const isIos = () => {
-        const userAgent = window.navigator.userAgent.toLowerCase();
-        return /iphone|ipad|ipod/.test( userAgent );
-      }
-      // Detects if device is in standalone mode
-      const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
-      
-      // Checks if should display install popup notification:
-      if (isIos() && !isInStandaloneMode()) {
-        this.setState({ showInstallMessage: true });
-      }
 
 ReactDOM.render(
 <Provider store={store}>
