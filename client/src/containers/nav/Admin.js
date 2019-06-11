1d33264651cb2c0ac432cfcb7d907b3e693ba445
nadjarben <nadjarbenj@gmail.com>
2019-06-12 01:30:00 +0200

    i am

--- client/src/containers/nav/Admin.js
+++ client/src/containers/nav/Admin.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import NavAdmin from '../../components/admin/nav//NavAdmin'
+import NavAdmin from '../../components/admin/nav//NavAdmin';
+import ScrollableTabsButtonPrevent from '../../components/admin/nav/NavAdmin2';
 ;
 const Admin = () => {
 
     return (
         <div>
             <NavAdmin />
+            <ScrollableTabsButtonPrevent />
         </div>
     )
 }
