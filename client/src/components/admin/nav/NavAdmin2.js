b658c91dddf797021243b70f94d836b9e6bf4be4
nadjarben <nadjarbenj@gmail.com>
2019-06-12 12:08:29 +0200

    i am

--- client/src/components/admin/nav/NavAdmin2.js
+++ client/src/components/admin/nav/NavAdmin2.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, withTheme } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom';
 
 function TabContainer(props) {
   return (
@@ -21,6 +22,7 @@ TabContainer.propTypes = {
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
+    font: 'withe',
     backgroundColor: theme.palette.background.paper,
   },
 }));
@@ -37,9 +39,9 @@ export default function SimpleTabs() {
     <div className={classes.root}>
       <AppBar position="static">
         <Tabs value={value} onChange={handleChange}>
-          <Tab label="Item One" />
-          <Tab label="Item Two" />
-          <Tab label="Item Three" />
+        <Link to="/home/admin/home"><Tab label="Item Two" /></Link>
+        <Link to="/home/admin/reservation"><Tab label="Item Two" /></Link>
+        <Link to="/home/admin/customer"><Tab label="Item Two" /></Link>
         </Tabs>
       </AppBar>
       {value === 0 && <TabContainer>Item One</TabContainer>}
