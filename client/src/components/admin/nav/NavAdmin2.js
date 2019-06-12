a9dcfdb1cc9482cb798a65322bb249f8a9d7d85e
nadjarben <nadjarbenj@gmail.com>
2019-06-12 11:59:04 +0200

    i am

--- client/src/components/admin/nav/NavAdmin2.js
+++ client/src/components/admin/nav/NavAdmin2.js
@@ -4,15 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import PhoneIcon from '@material-ui/icons/Phone';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import PersonPinIcon from '@material-ui/icons/PersonPin';
-import HelpIcon from '@material-ui/icons/Help';
-import ShoppingBasket from '@material-ui/icons/ShoppingBasket';
-import ThumbDown from '@material-ui/icons/ThumbDown';
-import ThumbUp from '@material-ui/icons/ThumbUp';
 import Typography from '@material-ui/core/Typography';
-import { Link } from 'react-router-dom';
 
 function TabContainer(props) {
   return (
@@ -29,12 +21,11 @@ TabContainer.propTypes = {
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
-    width: '100%',
     backgroundColor: theme.palette.background.paper,
   },
 }));
 
-export default function ScrollableTabsButtonPrevent() {
+export default function SimpleTabs() {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
 
@@ -45,23 +36,15 @@ export default function ScrollableTabsButtonPrevent() {
   return (
     <div className={classes.root}>
       <AppBar position="static">
-        <Tabs value={value} onChange={handleChange} variant="scrollable" scrollButtons="off">
-          <Link className="nav-link" to="/home/admin/adminhome">Home</Link>
-          <Tab icon={<FavoriteIcon />} aria-label="Favorite" />
-          <Tab icon={<PersonPinIcon />} aria-label="Person" />
-          <Tab icon={<HelpIcon />} aria-label="Help" />
-          <Tab icon={<ShoppingBasket />} aria-label="Shopping" />
-          <Tab icon={<ThumbDown />} aria-label="Up" />
-          <Tab icon={<ThumbUp />} aria-label="Down" />
+        <Tabs value={value} onChange={handleChange}>
+          <Tab label="Item One" />
+          <Tab label="Item Two" />
+          <Tab label="Item Three" />
         </Tabs>
       </AppBar>
       {value === 0 && <TabContainer>Item One</TabContainer>}
       {value === 1 && <TabContainer>Item Two</TabContainer>}
       {value === 2 && <TabContainer>Item Three</TabContainer>}
-      {value === 3 && <TabContainer>Item Four</TabContainer>}
-      {value === 4 && <TabContainer>Item Five</TabContainer>}
-      {value === 5 && <TabContainer>Item Six</TabContainer>}
-      {value === 6 && <TabContainer>Item Seven</TabContainer>}
     </div>
   );
 }
