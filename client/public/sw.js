2de2a8b3a5159f24d86d75ffdfb802d28040dc22
nadjarben <nadjarbenj@gmail.com>
2019-06-18 20:54:08 +0200

    i am

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,23 +1,12 @@
 
-const staticCacheName = 'site-static';
-const assets = [
-    '/',
-]
-
-
 self.addEventListener('install', evt => {
-    //console.log('service worker installed');
-    evt.waitUntil(
-        caches.open(staticCacheName).then(cache => {
-            console.log('caching shel assets');
-            cache.addAll(assets)
-    }))
+    console.log('service worker installed');
 });
 
 self.addEventListener('activate', evt => {
-    //console.log('service worker activated');
+    console.log('service worker activated');
 });
 
 self.addEventListener('fetch', evt => {
-    //console.log('event fetched', evt);
+    console.log('event fetched', evt);
 });
