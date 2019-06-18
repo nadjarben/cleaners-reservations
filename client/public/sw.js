d19f9df8144929f296977628638c2f063e67aa5b
nadjarben <nadjarbenj@gmail.com>
2019-06-18 18:34:08 +0200

    i am

---
+++ client/public/sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', evt => {
+    console.log('service worker installed');
+});
+
+self.addEventListener('activate', evt => {
+    console.log('service worker activated');
+});
