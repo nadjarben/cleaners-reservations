2409d864091436d33c2014c785ffee520ad2078b
nadjarben <nadjarbenj@gmail.com>
2019-06-18 12:21:25 +0200

    i am

--- client/src/config/registerServiceWorker.js
+++ client/src/config/registerServiceWorker.js
@@ -108,25 +108,6 @@ function checkValidServiceWorker(swUrl) {
     });
 }
 
-function showIosInstall() {
-  let iosPrompt = document.querySelector(".ios-prompt");
-  iosPrompt.style.display = "block";
-  iosPrompt.addEventListener("click", () => {
-    iosPrompt.style.display = "none";
-  });
-}
-
-const isIos = () => {
-  const userAgent = window.navigator.userAgent.toLowerCase();
-  return /iphone|ipad|ipod/.test( userAgent );
-}
-// Detects if device is in standalone mode
-const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
-// Checks if should display install popup notification:
-if (isIos() && !isInStandaloneMode()) {
-  // this.setState({ showInstallMessage: true });
-  showIosInstall();
-}
 
 export function unregister() {
   if ('serviceWorker' in navigator) {
