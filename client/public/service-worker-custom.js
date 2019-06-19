da798dc8f35998b2fae7edd217c1b470f63ba5a2
nadjarben <nadjarbenj@gmail.com>
2019-06-19 13:26:30 +0200

    i am

--- client/public/service-worker-custom.js
+++ client/public/service-worker-custom.js
@@ -1,1 +1,12 @@
-console.log('My custom service worker');
+
+self.addEventListener('install', evt => {
+   // console.log('service worker installed');
+});
+
+self.addEventListener('activate', evt => {
+    //console.log('service worker activated');
+});
+
+self.addEventListener('fetch', evt => {
+    console.log('event fetched', evt);
+});
