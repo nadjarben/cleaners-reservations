40f68cbdc9c0286a8808241def27a06e3f474404
nadjarben <nadjarbenj@gmail.com>
2019-07-15 12:31:58 +0300

    i am

--- client/src/containers/pages/HomePage.js
+++ client/src/containers/pages/HomePage.js
@@ -7,32 +7,34 @@ import Grid from '@material-ui/core/Grid';
 import { FormattedMessage } from 'react-intl'; 
 import banner from '../../images/banner.png'
 
-     const HomePage = () => {
-        return (
-            <div style={{backgroundImage:`url(${banner}`, height:'100%', width:'100%', position:'absolute'}}>
-                <div className='container'>
-                    <div className='row justify-content'>
-                        <div className='text-center'>
-                            <img src={logo} width='50%' alt="logoCleaners"></img>
-                        </div>
-                        <div className='text-center'>
-                            <img src={pressing} width='80%' alt="pressing"/>
-                        </div>
-                        <div className='text-center' style={{color:'white'}}>
-                            <p className="lead text-center"><FormattedMessage id="home.intro" defaultMessage=""/></p>
-                        </div>
-                        <div className= 'col'>
+     class HomePage extends React.Component{
+         render() {
+            return (
+                <div className='homepage' style={{backgroundImage:`url(${banner}`, width:'100%', position:'absolute'}}>
+                    <div className='container'>
+                        <div className='row justify-content'>
                             <div className='text-center'>
-                                <button className="bouton18">
-                                    <Link><p className="text-button"><FormattedMessage id="home.button" defaultMessage="BOOK A DELIVERY"/></p></Link>
-                                </button>
+                                <img src={logo} width='50%' alt="logoCleaners"></img>
+                            </div>
+                            <div className='text-center'>
+                                <img src={pressing} width='80%' alt="pressing"/>
+                            </div>
+                            <div className='text-center' style={{color:'white', marginTop:'-10%'}}>
+                                <p className="lead text-center"><FormattedMessage id="home.intro" defaultMessage=""/></p>
+                            </div>
+                            <div className= 'col'>
+                                <div className='text-center' style={{marginTop: '-10%'}}>
+                                    <button className="bouton18">
+                                        <Link><p className="text-button"><FormattedMessage id="home.button" defaultMessage="BOOK A DELIVERY"/></p></Link>
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            
-    );
+                
+        );
+    }
 }
 
 export default HomePage
