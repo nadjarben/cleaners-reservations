2830bd85656b9387ac6b74eb6b315834b71cc0b0
nadjarben <nadjarbenj@gmail.com>
2019-08-12 01:11:15 +0300

    i am

--- server.js
+++ server.js
@@ -63,6 +63,12 @@ if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build'));
   });
+
+  app.use(function(request, response){
+    if(!request.secure){
+      response.redirect("https://" + request.headers.host + request.url);
+    }
+  });
 }
 
 const port = process.env.PORT || 8080;
