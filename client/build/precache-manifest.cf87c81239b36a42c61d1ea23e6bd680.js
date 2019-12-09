dcbfe64b79332e7257c6fba25f5e90fdebd5f5db
nadjarben <nadjarbenj@gmail.com>
2019-12-10 00:44:03 +0200

    rm

---
+++ client/build/precache-manifest.cf87c81239b36a42c61d1ea23e6bd680.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08761f7a6efaab99fffbf3b1e7b1d60e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4bbe53bbc71d3d7904a",
+    "url": "/static/css/2.fa0c4f0f.chunk.css"
+  },
+  {
+    "revision": "fe866707ae43621709f9",
+    "url": "/static/css/main.e7227e01.chunk.css"
+  },
+  {
+    "revision": "a4bbe53bbc71d3d7904a",
+    "url": "/static/js/2.6b569a5f.chunk.js"
+  },
+  {
+    "revision": "fe866707ae43621709f9",
+    "url": "/static/js/main.35d0c518.chunk.js"
+  },
+  {
+    "revision": "40aa1fea39977e55cd7b",
+    "url": "/static/js/runtime-main.265dbd0d.js"
+  },
+  {
+    "revision": "eb52c63c14ffb4e7e960a9a4c89b2484",
+    "url": "/static/media/account-plus.eb52c63c.svg"
+  },
+  {
+    "revision": "9600b3421908f38fa45518f820c8b81a",
+    "url": "/static/media/carpet.9600b342.jpg"
+  },
+  {
+    "revision": "7f9a6c1e3d46a6aa0f73b6c53337eda0",
+    "url": "/static/media/coat.7f9a6c1e.jpg"
+  },
+  {
+    "revision": "606535b72f7ba6ab1c472da2a6257287",
+    "url": "/static/media/curtain.606535b7.jpg"
+  },
+  {
+    "revision": "2e3b3e7d4c0e4c0cc6133c35e4af2c3c",
+    "url": "/static/media/dress.2e3b3e7d.jpg"
+  },
+  {
+    "revision": "1616e0538c750bc4ef361c8351d4c6d4",
+    "url": "/static/media/duvet.1616e053.jpg"
+  },
+  {
+    "revision": "5f65d7323b027d51aa92705b0143e9af",
+    "url": "/static/media/flag-en.5f65d732.png"
+  },
+  {
+    "revision": "26614f91775dc0cacbadfc0fc99a4f02",
+    "url": "/static/media/flag-fr.26614f91.png"
+  },
+  {
+    "revision": "8948b4e5ee31b577afba0326217bf36a",
+    "url": "/static/media/flag-he.8948b4e5.png"
+  },
+  {
+    "revision": "e75210b578d62696c8174285a46c68f1",
+    "url": "/static/media/ironingdresses.e75210b5.jpg"
+  },
+  {
+    "revision": "9b7974f167524c3e12d91e91f3f803d3",
+    "url": "/static/media/laundry.9b7974f1.jpg"
+  },
+  {
+    "revision": "ede94db834ab8538ed7b32b7b22d3df5",
+    "url": "/static/media/logo-min.ede94db8.png"
+  },
+  {
+    "revision": "d3dba7878e92f70ff909f33da80379de",
+    "url": "/static/media/logo1.d3dba787.png"
+  },
+  {
+    "revision": "a3d238acd9d72bebd335844e01a4b0dd",
+    "url": "/static/media/money.a3d238ac.png"
+  },
+  {
+    "revision": "0a5133c89ed0f2c3f1463d21a4640ecc",
+    "url": "/static/media/plus-circle.0a5133c8.svg"
+  },
+  {
+    "revision": "7c773c26853bea7d02e18baba84b6419",
+    "url": "/static/media/shirt.7c773c26.jpg"
+  },
+  {
+    "revision": "a78706ab475b51b17431568b5c96c50c",
+    "url": "/static/media/suit.a78706ab.jpg"
+  },
+  {
+    "revision": "a6ed45cda67393164b18a9bdb0e3c867",
+    "url": "/static/media/tools.a6ed45cd.png"
+  }
+]);
--- client/src/index.js
+++ client/src/index.js
@@ -32,7 +32,7 @@ if (rootElement.hasChildNodes()) {
     </Provider>, rootElement);
 }
 
-registerServiceWorker();
+//registerServiceWorker();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
